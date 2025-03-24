d6473e2ce194e6b9aee27ba68b233ead02ac7742
serkanyurttapan <serkan.yurttapan@farmasi.com.tr>
2025-03-24 23:36:27 +0300

    Initial commit

---
+++ Producer/Sender.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+public class Sender
+{
+    public static void Main(string[] arg)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+        };
+        using (var connection = factory.CreateConnection())
+        using ( var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "BasicTest", null, body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }    
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
